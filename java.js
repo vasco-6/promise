d063f555a919834c774d87340a87456a2fa7f9ef
Theo <vascoampah@gmail.com>
2025-01-22 18:59:52 +0000

    New info for promise in both reject and resolve

--- java.js
+++ java.js
@@ -1,1 +1,32 @@
-//
+// FETCH API requires a discussion of ...
+//Callbacks, Promises, Thenables, Asych/Awaits
+
+//PROMISES
+
+// STATE 3: PENDING, FULFILLED, REJECT
+
+
+const MyNewPromise = new Promise((resolve, reject) => {
+    const error = false;
+    if (!error) {
+        resolve("Yes resolved the Promise");
+    } else {
+        reject("No! rejected the Promise");
+    }
+});
+
+const MyNextPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("MyNextPromise is resolved");
+    }, 3000);
+});
+
+MyNextPromise.then(value => {
+    console.log(value);
+});
+
+MyNewPromise.then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
